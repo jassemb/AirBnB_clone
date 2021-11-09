77e1f0dcf1d364fc2308a710e7a92ca2d0f96b61
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 <benalijassem201@gmail.com>
2021-11-09 04:59:51 -0800

    models function

---
+++ models/base_model.py
@@ -0,0 +1,52 @@
+#!/usr/bin/python3
+"""This module creates the Base class"""
+
+import uuid
+from datetime import datetime
+import models
+
+
+class BaseModel:
+    """
+    class BaseModel that defines all common
+    attributes/methods for other classes
+    """
+    def __init__(self, *args, **kwargs):
+        if(len(kwargs) == 0):
+            self.id = str(uuid.uuid4())
+            self.created_at = datetime.now()
+            self.updated_at = datetime.now()
+            models.storage.new(self)
+            models.storage.save()
+        else:
+            kwargs["created_at"] = datetime.strptime(kwargs["created_at"],
+                                                     "%Y-%m-%dT%H:%M:%S.%f")
+            kwargs["updated_at"] = datetime.strptime(kwargs["updated_at"],
+                                                     "%Y-%m-%dT%H:%M:%S.%f")
+            for key in kwargs:
+                if key != "__class__":
+                    setattr(self, key, kwargs[key])
+
+    def __str__(self):
+        """Returns the informal representation"""
+        class_name = self.__class__.__name__
+        return"[{}] ({}) {}".format(class_name, self.id, self.__dict__)
+
+    def save(self):
+        """
+        updates the public instance
+        attribute updated_at with the current datetime
+        """
+        self.updated_at = datetime.now()
+        models.storage.save()
+
+    def to_dict(self):
+        """
+        returns a dictionary containing all
+        keys/values of __dict__ of the instance
+        """
+        dic = dict(**self.__dict__)
+        dic["__class__"] = self.__class__.__name__
+        dic["created_at"] = self.created_at.isoformat()
+        dic["updated_at"] = self.updated_at.isoformat()
+        return dic
