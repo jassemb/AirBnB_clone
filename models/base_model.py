1538aa07dcc378ba509ce4c80b160f9fbc6e8f0e
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 <benalijassem201@gmail.com>
2021-11-13 11:23:06 -0800

    fix

--- models/base_model.py
+++ models/base_model.py
@@ -32,7 +32,7 @@ class BaseModel:
 
     def __str__(self):
         """
-        str methode
+        should print: [<class name>] (<self.id>) <self.__dict__>
         """
         return "[{}] ({}) {}".format(self.__class__.__name__, self.id,
                                      self.__dict__)
